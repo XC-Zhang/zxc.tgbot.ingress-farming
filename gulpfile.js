00013f81abd2d08820816abbabf171ccbc0febab
xc-zhang-office <studentzxc@live.cn>
2018-05-25 13:43:29 +0800

    Use gulp to pack the project.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+
+gulp.task("release", function () {
+    const tar = require("gulp-tar");
+    const gzip = require("gulp-gzip");
+    const globs = [
+        "./index.js",
+        "./package.json",
+        "./yarn.lock"
+    ]
+    gulp.src(globs)
+        .pipe(tar("release.tar"))
+        .pipe(gzip())
+        .pipe(gulp.dest("."));
+});
