d5ffa3117f2c2166c2ab5390a5f8c16f0e2c0efa
xc-zhang-office <studentzxc@live.cn>
2018-05-31 16:39:20 +0800

    Generate sourcemaps for debugging.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,16 @@ const gulp = require("gulp");
 
 gulp.task("tsc", function () {
     const ts = require("gulp-typescript");
+    const sourcemaps = require("gulp-sourcemaps");
     const project = ts.createProject("tsconfig.json");
     return project.src()
+        .pipe(sourcemaps.init())
         .pipe(project())
         .js
+        .pipe(sourcemaps.write(".", {
+            includeContent: false,
+            sourceRoot: "../src"
+        }))
         .pipe(gulp.dest("dist"));
 });
 
